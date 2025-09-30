c69ca39e75a9ad535a9c9fb2b5a8e7f0e8fa4991
Gabriel Kumlehn <gskumlehn@gmail.com>
2025-09-30 18:11:32 -0300

    Add cors lovable

--- app/controllers/lovable_controller.py
+++ app/controllers/lovable_controller.py
@@ -1,7 +1,21 @@
 from flask import Blueprint, jsonify, request, Response
+from flask_cors import cross_origin
 from ..services.lovable_service import LovableService
 
 lovable_bp = Blueprint("lovable", __name__)
+
+@lovable_bp.after_request
+def after_request(response):
+    origin = request.headers.get('Origin')
+    if origin and (
+        origin.endswith('.lovableproject.com') or 
+        origin.endswith('.lovable.app')
+    ):
+        response.headers['Access-Control-Allow-Origin'] = origin
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
 
 @lovable_bp.get("/assets")
@@ -39,6 +53,6 @@ def webfonts_css():
     svc = LovableService()
     css = svc.generate_webfonts_css(
         brand_name = brand_name,
-        prefer_stream = True  # usa /stream para evitar CORS
+        prefer_stream = True
     )
     return Response(css, mimetype="text/css", status=200)
